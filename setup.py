3399d64a416c451837f1faebf4ce523e29d07874
Stephen Roller <roller@cs.utexas.edu>
2011-06-07 16:33:27 -0400

    Add a setup script for people to install with.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Hyperpublic",
+      version="0.1",
+      description="Wrapper around the Hyperpublic API",
+      author="Stephen Roller",
+      author_email="stephen@hyperpublic.com",
+      url="http://developer.hyperpublic.com/",
+      py_modules=['hyperpublic'],
+      )
